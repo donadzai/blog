38afc9195b8f7da2d9d373039381db92e6e001aa
donadzai <kieuduydoan2k2@gmail.com>
2023-10-30 00:47:41 +0700

    Static file & SCSS

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
+//HTTP logger
 app.use(morgan("combined"));
 
+///Template engine
 app.engine(".hbs", handlebars.engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resourses/views"));
