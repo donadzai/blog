0121081970e6039b743409cf94038f5b87912f72
donadzai <kieuduydoan2k2@gmail.com>
2023-10-29 23:43:29 +0700

    Template engine (handlebars)

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+app.use(morgan("combined"));
+
+app.engine(".hbs", handlebars.engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "resourses/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
