6ea1c20f8753fbbe9a596796c455986a54860f68
donadzai <kieuduydoan2k2@gmail.com>
2023-11-02 00:16:50 +0700

    Form default behavior

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
-const express = require("express");
-const handlebars = require("express-handlebars");
-const path = require("path");
-const morgan = require("morgan");
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //HTTP logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 ///Template engine
-app.engine(".hbs", handlebars.engine({ extname: ".hbs" }));
-app.set("view engine", ".hbs");
-app.set("views", path.join(__dirname, "resourses/views"));
+app.engine('.hbs', handlebars.engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resourses/views'));
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
